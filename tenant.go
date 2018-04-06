6299ce6ca32dd48b393c3f06b5098c538591ae88
taiyoh <sun.basix@gmail.com>
2018-04-06 14:58:45 +0900

    add tenant model and its test

--- tenant.go
+++ tenant.go
@@ -1,9 +1,65 @@
 package labeltile
 
-// Tenant manages labels
+import "errors"
+
+// Tenant manages langs and categories
 type Tenant struct {
 	ID          TenantID
+	Name        string
 	DefaultLang LangID
 	Languages   []LangID
-	Categories  []*Category
+	Categories  []CategoryID
+}
+
+// NewTenant returns initialized Tenant object
+func NewTenant(id TenantID, name string, dl LangID) *Tenant {
+	return &Tenant{
+		ID:          id,
+		Name:        name,
+		DefaultLang: dl,
+		Languages:   []LangID{dl},
+	}
+}
+
+// AddLanguage set new language to use
+func (t *Tenant) AddLanguage(l LangID) {
+	t.Languages = append(t.Languages, l)
+}
+
+// DeleteLanguage unset language for unuse
+func (t *Tenant) DeleteLanguage(l LangID) {
+	langs := []LangID{}
+	for _, ln := range t.Languages {
+		if ln != l {
+			langs = append(langs, ln)
+		}
+	}
+	t.Languages = langs
+}
+
+// ChangeDefaultLang set default language for tenant
+func (t *Tenant) ChangeDefaultLang(l LangID) error {
+	for _, ln := range t.Languages {
+		if ln == l {
+			t.DefaultLang = l
+			return nil
+		}
+	}
+	return errors.New("specified lang is not registered")
+}
+
+// AddCategory set new category for managing in tenant
+func (t *Tenant) AddCategory(id CategoryID) {
+	t.Categories = append(t.Categories, id)
+}
+
+// DeleteCategory unset category
+func (t *Tenant) DeleteCategory(id CategoryID) {
+	cats := []CategoryID{}
+	for _, ca := range t.Categories {
+		if ca != id {
+			cats = append(cats, ca)
+		}
+	}
+	t.Categories = cats
 }
---
+++ tenant_test.go
@@ -0,0 +1,63 @@
+package labeltile
+
+import "testing"
+
+func TestTenant(t *testing.T) {
+	tenant := NewTenant(TenantID("1"), "foo", LangID("ja"))
+	if tenant.Name != "foo" {
+		t.Error("tenant name should be 'foo'")
+	}
+	if tenant.DefaultLang != LangID("ja") {
+		t.Error("tenant default lang is not set")
+	}
+	if len(tenant.Languages) != 1 {
+		t.Error("tenant langs should be default lang only")
+	}
+	if tenant.Languages[0] != LangID("ja") {
+		t.Error("tenant langs is wrong")
+	}
+
+	tenant.AddLanguage(LangID("en"))
+	if len(tenant.Languages) != 2 {
+		t.Error("lang:en should be added")
+	}
+	if tenant.Languages[1] != LangID("en") {
+		t.Error("lang:en should be added")
+	}
+
+	if err := tenant.ChangeDefaultLang(LangID("fr")); err == nil {
+		t.Error("lang:fr is not set")
+	}
+	if err := tenant.ChangeDefaultLang(LangID("en")); err != nil {
+		t.Error("lang:en should be set to default lang")
+	}
+	if tenant.DefaultLang != LangID("en") {
+		t.Error("lang:en should be set to default lang")
+	}
+	tenant.DeleteLanguage(LangID("ja"))
+	if len(tenant.Languages) != 1 {
+		t.Error("lang:ja should be removed")
+	}
+	if tenant.Languages[0] != LangID("en") {
+		t.Error("lang:ja should be removed")
+	}
+
+	tenant.AddCategory(CategoryID("p1"))
+	tenant.AddCategory(CategoryID("p2"))
+	if len(tenant.Categories) != 2 {
+		t.Error("category:p1 and category:p2 should be added")
+	}
+	if tenant.Categories[0] != CategoryID("p1") {
+		t.Error("category:p1 should be added")
+	}
+	if tenant.Categories[1] != CategoryID("p2") {
+		t.Error("category:p2 should be added")
+	}
+	tenant.DeleteCategory(CategoryID("p1"))
+	if len(tenant.Categories) != 1 {
+		t.Error("category:p1 should be removed")
+	}
+	if tenant.Categories[0] != CategoryID("p2") {
+		t.Error("category:p2 should be set")
+	}
+}
