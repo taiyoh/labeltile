23522708fc243bcc1d92e701ae47ff02b4571428
taiyoh <sun.basix@gmail.com>
2018-04-06 12:45:39 +0900

    creating domain model

---
+++ label.go
@@ -0,0 +1,78 @@
+package labeltile
+
+import (
+	"time"
+)
+
+// Label manages sentence by lang
+type Label struct {
+	ID        LabelID
+	Key       string
+	Note      string
+	Category  CategoryID
+	Active    bool
+	Sentences map[LangID]*langSentence
+	CreatedAt time.Time
+}
+
+type langSentence struct {
+	Lang             LangID
+	Sentence         string
+	IsVerified       bool
+	LastUpdatedAt    time.Time
+	LastUpdatedUser  UserID
+	LastVerifiedAt   time.Time
+	LastVerifiedUser UserID
+}
+
+// NewLabel returns initialized Label object
+func NewLabel(key string, catID CategoryID) *Label {
+	return &Label{
+		Key:       key,
+		Category:  catID,
+		Active:    true,
+		CreatedAt: time.Now(),
+	}
+}
+
+// FillLangSentence stores sentence by lang. IsVerified flag is forced to set false
+func (l *Label) FillLangSentence(ln LangID, s string, u UserID) {
+	if ls, exists := l.Sentences[ln]; exists {
+		l.Sentences[ln] = &langSentence{
+			Lang:             ln,
+			Sentence:         s,
+			LastUpdatedAt:    time.Now(),
+			LastUpdatedUser:  u,
+			LastVerifiedAt:   ls.LastVerifiedAt,
+			LastVerifiedUser: ls.LastVerifiedUser,
+		}
+	} else {
+		l.Sentences[ln] = &langSentence{
+			Lang:            ln,
+			Sentence:        s,
+			LastUpdatedAt:   time.Now(),
+			LastUpdatedUser: u,
+		}
+	}
+}
+
+// VerifyByLang exchange IsVerified flag to true
+func (l *Label) VerifyByLang(ln LangID, u UserID) bool {
+	if ls, exists := l.Sentences[ln]; exists {
+		ls.IsVerified = true
+		ls.LastVerifiedAt = time.Now()
+		ls.LastVerifiedUser = u
+		return true
+	}
+	return false
+}
+
+// Activate set Active flag to true
+func (l *Label) Activate() {
+	l.Active = true
+}
+
+// Deactivate set Active flag to false
+func (l *Label) Deactivate() {
+	l.Active = false
+}
---
+++ types.go
@@ -0,0 +1,13 @@
+package labeltile
+
+// UserID is alias for identifing user
+type UserID string
+
+// CategoryID is alias for identifing category
+type CategoryID string
+
+// LabelID is alias for identifing label
+type LabelID string
+
+// LangID is alias for identifing lang
+type LangID string
