40fa980cfc79ff17b7a2272c6bccf6481b3e0f5c
taiyoh <sun.basix@gmail.com>
2018-04-07 11:05:50 +0900

    add comment

--- auth/domain/user.go
+++ auth/domain/user.go
@@ -1,21 +1,25 @@
 package domain
 
+// User is model for accsessing account
 type User struct {
 	ID    UserID
 	Roles []UserRoleID
 }
 
+// UserRole is relation model for user and permission
 type UserRole struct {
 	ID          UserRoleID
 	Name        string
 	Permissions []UserPermissionID
 }
 
+// UserPermission is model for permission of user's action
 type UserPermission struct {
 	ID   UserPermissionID
 	Name string
 }
 
+// NewUser returns initialized user object
 func NewUser(id UserID, role UserRoleID) *User {
 	return &User{
 		ID:    id,
@@ -23,10 +27,12 @@ func NewUser(id UserID, role UserRoleID) *User {
 	}
 }
 
+// AddRole set role to user
 func (u *User) AddRole(r UserRoleID) {
 	u.Roles = append(u.Roles, r)
 }
 
+// DeleteRole unset role from user
 func (u *User) DeleteRole(r UserRoleID) {
 	rolelist := []UserRoleID{}
 	for _, ur := range u.Roles {
