7bdf9a49a23b38eba486bf12cf4ed1244cb74bfd
taiyoh <sun.basix@gmail.com>
2018-08-07 13:58:45 +0900

    separate package for Tenant and its test

--- app/domain/tenant_test.go
+++ app/domain/tenant_test.go
@@ -1,63 +1,67 @@
-package domain
+package domain_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/taiyoh/labeltile/app/domain"
+)
 
 func TestTenant(t *testing.T) {
-	tenant := NewTenant(TenantID("1"), "foo", LangID("ja"))
+	tenant := domain.NewTenant(domain.TenantID("1"), "foo", domain.LangID("ja"))
 	if tenant.Name != "foo" {
 		t.Error("tenant name should be 'foo'")
 	}
-	if tenant.DefaultLang != LangID("ja") {
+	if tenant.DefaultLang != domain.LangID("ja") {
 		t.Error("tenant default lang is not set")
 	}
 	if len(tenant.Languages) != 1 {
 		t.Error("tenant langs should be default lang only")
 	}
-	if tenant.Languages[0] != LangID("ja") {
+	if tenant.Languages[0] != domain.LangID("ja") {
 		t.Error("tenant langs is wrong")
 	}
 
-	tenant.AddLanguage(LangID("en"))
+	tenant.AddLanguage(domain.LangID("en"))
 	if len(tenant.Languages) != 2 {
 		t.Error("lang:en should be added")
 	}
-	if tenant.Languages[1] != LangID("en") {
+	if tenant.Languages[1] != domain.LangID("en") {
 		t.Error("lang:en should be added")
 	}
 
-	if err := tenant.ChangeDefaultLang(LangID("fr")); err == nil {
+	if err := tenant.ChangeDefaultLang(domain.LangID("fr")); err == nil {
 		t.Error("lang:fr is not set")
 	}
-	if err := tenant.ChangeDefaultLang(LangID("en")); err != nil {
+	if err := tenant.ChangeDefaultLang(domain.LangID("en")); err != nil {
 		t.Error("lang:en should be set to default lang")
 	}
-	if tenant.DefaultLang != LangID("en") {
+	if tenant.DefaultLang != domain.LangID("en") {
 		t.Error("lang:en should be set to default lang")
 	}
-	tenant.DeleteLanguage(LangID("ja"))
+	tenant.DeleteLanguage(domain.LangID("ja"))
 	if len(tenant.Languages) != 1 {
 		t.Error("lang:ja should be removed")
 	}
-	if tenant.Languages[0] != LangID("en") {
+	if tenant.Languages[0] != domain.LangID("en") {
 		t.Error("lang:ja should be removed")
 	}
 
-	tenant.AddCategory(CategoryID("p1"))
-	tenant.AddCategory(CategoryID("p2"))
+	tenant.AddCategory(domain.CategoryID("p1"))
+	tenant.AddCategory(domain.CategoryID("p2"))
 	if len(tenant.Categories) != 2 {
 		t.Error("category:p1 and category:p2 should be added")
 	}
-	if tenant.Categories[0] != CategoryID("p1") {
+	if tenant.Categories[0] != domain.CategoryID("p1") {
 		t.Error("category:p1 should be added")
 	}
-	if tenant.Categories[1] != CategoryID("p2") {
+	if tenant.Categories[1] != domain.CategoryID("p2") {
 		t.Error("category:p2 should be added")
 	}
-	tenant.DeleteCategory(CategoryID("p1"))
+	tenant.DeleteCategory(domain.CategoryID("p1"))
 	if len(tenant.Categories) != 1 {
 		t.Error("category:p1 should be removed")
 	}
-	if tenant.Categories[0] != CategoryID("p2") {
+	if tenant.Categories[0] != domain.CategoryID("p2") {
 		t.Error("category:p2 should be set")
 	}
 }
