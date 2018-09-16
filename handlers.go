0dc5505abce4064927701e08029d4fa27bab7e54
taiyoh <sun.basix@gmail.com>
2018-09-16 11:25:07 +0900

    modify oauth handler implementation

--- handlers.go
+++ handlers.go
@@ -1,10 +1,14 @@
 package labeltile
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha256"
+	"hash"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/graphql-go/graphql"
 	"github.com/taiyoh/labeltile/app"
 )
@@ -22,16 +26,41 @@ func (h *HomeHandler) Top(c *gin.Context) {
 // OAuth2GoogleHandler provides controllers for oauth authorization by google
 type OAuth2GoogleHandler struct {
 	container app.Container
+	sha256    hash.Hash
+}
+
+func (h *OAuth2GoogleHandler) generateState() string {
+	return bytes.NewBuffer(h.sha256.Sum(uuid.New().NodeID())).String()
 }
 
 // Access is controller for oauth page redirection to google
 func (h *OAuth2GoogleHandler) Access(c *gin.Context) {
 	o := h.container.OAuth2Google()
-	c.Redirect(http.StatusTemporaryRedirect, o.AuthCodeURL(""))
+	c.Redirect(http.StatusTemporaryRedirect, o.AuthCodeURL(h.generateState()))
 }
 
 // Callback is controller for rediret page from google when authorized
-func (h *OAuth2GoogleHandler) Callback(c *gin.Context) {}
+func (h *OAuth2GoogleHandler) Callback(c *gin.Context) {
+	s, _ := c.Get("session")
+	session, _ := s.(app.SessionData)
+	r := c.Request
+	if err := r.ParseForm(); err != nil {
+		session.Set("errors", []string{"cannot parse query"})
+		c.Redirect(http.StatusTemporaryRedirect, "/")
+		return
+	}
+	code := r.FormValue("code")
+
+	user, err := app.UserAuthorizeService(code, h.container)
+	if err != nil {
+		session.Set("errors", []string{"fetching token failed"})
+		c.Redirect(http.StatusTemporaryRedirect, "/")
+		return
+	}
+
+	c.Set("userID", user.ID)
+	c.Redirect(http.StatusTemporaryRedirect, "/")
+}
 
 // GraphQLHandler provides controllers for graphql requests
 type GraphQLHandler struct {
@@ -74,7 +103,7 @@ func SetupHomeHandler(router *gin.Engine, container app.Container) {
 
 // SetupOAuth2GoogleHandler provides initializing OAuth2GoogleHandler and binding its controller and endpoint
 func SetupOAuth2GoogleHandler(router *gin.Engine, container app.Container) {
-	h := &OAuth2GoogleHandler{container: container}
+	h := &OAuth2GoogleHandler{container: container, sha256: sha256.New()}
 	router.GET("/auth/google", h.Access)
 	router.GET("/auth/google/callback", h.Callback)
 }
